6c4a22d907b2f923a33930961cfb680013ce2b0a
Xuanchen Tang <laughmetal@gmail.com>
2015-05-30 16:21:57 -0400

    Update Indent

--- 2014/QualificationRound/CookieClickerAlpha/CookieClickerAlpha.cpp
+++ 2014/QualificationRound/CookieClickerAlpha/CookieClickerAlpha.cpp
@@ -12,28 +12,28 @@
 using namespace std;
 
 double leastTime(double C, double F, double X, double speed) {
-	if (abs((X / speed) - ((C / speed) + X / (speed + F))) < 1e-7) {
-		return X / speed;
-	} else {
-		return C / speed + leastTime(C, F, X, speed + F);
-	}
+    if (abs((X / speed) - ((C / speed) + X / (speed + F))) < 1e-7) {
+        return X / speed;
+    } else {
+        return C / speed + leastTime(C, F, X, speed + F);
+    }
 }
 
 int main() {
-	int caseNumber;
-	cin >> caseNumber;
+    int caseNumber;
+    cin >> caseNumber;
 
-	int i;
-	double C, F, X;
-	double result;
+    int i;
+    double C, F, X;
+    double result;
 
-	cout.precision(7);
-	cout << fixed;
-	for (i = 0; i < caseNumber; ++i) {
-		cin >> C >> F >> X;
-		result = leastTime(C, F, X, 2.0f);
-		cout << "Case #" << i + 1 << ": " << result << endl;
-	}
+    cout.precision(7);
+    cout << fixed;
+    for (i = 0; i < caseNumber; ++i) {
+        cin >> C >> F >> X;
+        result = leastTime(C, F, X, 2.0f);
+        cout << "Case #" << i + 1 << ": " << result << endl;
+    }
 
-	return 0;
+    return 0;
 }
--- 2014/QualificationRound/MinesweeperMaster/MinesweeperMaster.cpp
+++ 2014/QualificationRound/MinesweeperMaster/MinesweeperMaster.cpp
@@ -15,62 +15,62 @@ using namespace std;
 
 // Did NOT finish..
 int main() {
-	int caseNumber;
-	cin >> caseNumber;
+    int caseNumber;
+    cin >> caseNumber;
 
-	int R, C, M;
-	int i, j, base, space, count;
-	vector<int> length, counter;
-	for (i = 0; i < caseNumber; ++i) {
-		cin >> R >> C >> M;
-		space = R * C - M;
+    int R, C, M;
+    int i, j, base, space, count;
+    vector<int> length, counter;
+    for (i = 0; i < caseNumber; ++i) {
+        cin >> R >> C >> M;
+        space = R * C - M;
 
-		if (min(R, C) == 1) {
-			cout << 'c' << endl;
-			for (j = 0; j < space - 1; ++j) {
-				cout << '.' << endl;
-			}
-			for (j = 0; j < M; ++j) {
-				cout << '*' << endl;
-			}
-			continue;
-		}
+        if (min(R, C) == 1) {
+            cout << 'c' << endl;
+            for (j = 0; j < space - 1; ++j) {
+                cout << '.' << endl;
+            }
+            for (j = 0; j < M; ++j) {
+                cout << '*' << endl;
+            }
+            continue;
+        }
 
-		base = min(R, C);
+        base = min(R, C);
 
-		while (space > 1) {
-			count = space / base;
-			if ((space % base) == 1) {
-				if (count > 1) {
-					length.push_back(base);
-					counter.push_back(count - 1);
-					space -= (count - 1) * base;
-					--base;
-				} else {
-					break;
-				}
-			} else {
-				length.push_back(base);
-				counter.push_back(count);
-				space -= count * base;
-				--base;
-			}
-			if (space == 1) {
-				length.push_back(1);
-				counter.push_back(1);
-			}
-		}
+        while (space > 1) {
+            count = space / base;
+            if ((space % base) == 1) {
+                if (count > 1) {
+                    length.push_back(base);
+                    counter.push_back(count - 1);
+                    space -= (count - 1) * base;
+                    --base;
+                } else {
+                    break;
+                }
+            } else {
+                length.push_back(base);
+                counter.push_back(count);
+                space -= count * base;
+                --base;
+            }
+            if (space == 1) {
+                length.push_back(1);
+                counter.push_back(1);
+            }
+        }
 
-		if (counter.size() == 0) {
-			cout << "Case #" << i + 1 << ":\nImpossible" << endl;
-			continue;
-		}
+        if (counter.size() == 0) {
+            cout << "Case #" << i + 1 << ":\nImpossible" << endl;
+            continue;
+        }
 
-		if (accumulate(counter.begin(), counter.end(), 0) > max(R, C)) {
-			cout << "Case #" << i + 1 << ":\nImpossible" << endl;
-			continue;
-		}
-	}
+        if (accumulate(counter.begin(), counter.end(), 0) > max(R, C)) {
+            cout << "Case #" << i + 1 << ":\nImpossible" << endl;
+            continue;
+        }
+    }
 
-	return 0;
+    return 0;
 }
